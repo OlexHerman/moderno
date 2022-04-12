287e44b1a68266c6744171fefa8c4c259e431d3d
OlexHerman <olexherman@gmail.com>
2022-04-12 20:15:32 +0300

    Added product-slider -- product-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function(){
   var mixer = mixitup('.products__inner-box');
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    readOnly: true,
+    starWidth: "12px"
+  });
+
+  $(".product-slider__inner").slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+
+  })
+
 });
+
+
+
 
 
